1dd028ea8b6fccf543be3f8d267e0a57e4e47a1f
Ruslan Novikov <ruslanvnovik0@gmail.com>
2026-08-13 02:07:30 +0300

    feat: add icon and add it to index

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -12,3 +12,4 @@ export { TrashIcon } from './icons/TrashIcon';
 export { TruckIcon } from './icons/TruckIcon';
 export { UserIcon } from './icons/UserIcon';
 export { WhatsAppIcon } from './icons/WhatsAppIcon';
+export { ContactCardIcon } from './icons/ContactCardIcon';
